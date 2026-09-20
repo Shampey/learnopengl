1b1ede4256739e31844619679de9c09c2018bafa
Wilde Truex <wildetruex@gmail.com>
2026-09-20 17:41:48 -0600

    man like it works now i guess

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-cd build && make -j$(sysctl -n hw.logicalcpu) && ./LearnOpenGL
+#!/bin/sh
+set -e
+
+# Configure the native macOS preset (architecture is auto-detected:
+# arm64 on Apple Silicon, x86_64 on Intel).
+cmake --preset macos-native
+
+# Build and run.
+cmake --build --preset macos-native -j"$(sysctl -n hw.logicalcpu)"
+./build/LearnOpenGL
